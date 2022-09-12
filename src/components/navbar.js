bd5f6bdcc6245e4cf8b624330ebe86c94c87ca25
Chris Laneville <chris.laneville@fdmgroup.com>
2022-09-11 22:27:47 -0400

    final changes 9-11-22

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import './navbar.css';
-import LinkedInIcon from '../icons/linkedInIcon.png';
+import emailIcon from '../icons/emailIcon.jpg';
+import linkedInIcon from '../icons/linkedInIcon.png';
+import trailheadIcon from '../icons/trailheadIcon.png';
 import { Link } from "react-router-dom";
 const Navbar = () => {
-
+  const myRefname= useRef(null);
   const selectTab = (e) => {
 
     e.preventDefault();
@@ -20,7 +22,6 @@ const Navbar = () => {
       <div className="nameAndTabs">
         <div className="nameAndContactInfo">
           <h1 className="name">Christopher Laneville</h1>
-          <image src={LinkedInIcon}/>
           <ul className="contactList">
             <li className="contactLink">
               <a href="mailto:lanevillec@gmail.com" style={{textDecoration: 'none'}} >email</a>
@@ -29,7 +30,7 @@ const Navbar = () => {
               <a rel="noreferrer" href="https://www.linkedin.com/in/chrislaneville" target="_blank" style={{textDecoration: 'none'}} >linkedIn</a>
             </li>
             <li className="contactLink">
-              <a  rel="noreferrer" href="https://trailblazer.me/id/lanevillec" target="_blank" style={{textDecoration: 'none'}} >trailhead</a>
+              <a rel="noreferrer" href="https://trailblazer.me/id/lanevillec" target="_blank" style={{textDecoration: 'none'}} >trailhead</a>
             </li>
           </ul>
         </div>
@@ -44,7 +45,7 @@ const Navbar = () => {
           </li>
           <li id='Interests' className="tab" onClick={selectTab}>
             {/* Endpoint to route to Interests component */}
-            <Link id='Interests' name='Interests' style={{textDecoration: 'none'}} to="/interests">Interests</Link>
+            <Link id='Interests' name='Interests' style={{textDecoration: 'none'}} to="/interests">Personal</Link>
           </li>
         </ul>
         </div>
--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import '../components/navbar.css';
+import SFAdmin from '../icons/SFAdmin.png';
+import SFAdvAdmin from '../icons/SFAdvAdmin.png';
+import SFDevI from '../icons/SFDevI.png';
+import SFDevII from '../icons/SFDevII.png';
+import SFJScript from '../icons/SFJScript.png';
+import SFPlatAppBuilder from '../icons/SFPlatAppBuilder.png';
 const AboutMe = () =>{
   return (
     <div className="aboutMePage">
@@ -7,27 +13,45 @@ const AboutMe = () =>{
         <h2>Salesforce developer-scrum master-project manager with over 3.5 years of experience delivering innovative, thoughtful, and scalable solutions on the lightning platform.</h2>
       </div>
       <div className="aboutMeComp skills">
-        <h2>Skills</h2>
+        <h2>I am fluent in...</h2>
         <ul className="skillList">
-          <li> Salesforce Configuration: Custom Objects, Formulas, Validation Rules, Profiles, Permission 
+          <li className="skill"> Salesforce Configuration: Custom Objects, Formulas, Validation Rules, Profiles, Permission 
 Sets, Workflows, Approval Processes, Email Templates, Process Builder, Flows</li>
-          <li>Apex: Classes, Interfaces, Triggers, Batch Apex, Queueable, Callouts, etc</li>
-          <li>Integration: REST API, SOAP API, Bulk API, Metadata API, Webservices, Informatica</li>
-          <li>User Interface: Visualforce, Aura Components, Lightning Web Components, SLDS</li>
-          <li>Reporting: Tableau, Conga, Salesforce Reports/Dashboards</li>
-          <li>Design Patterns: coding best practices, bulkification, fflib, service classes, test factories, 
+          <li className="skill">Apex: Classes, Interfaces, Triggers, Batch Apex, Queueable, Callouts, etc</li>
+          <li className="skill">Integration: REST API, SOAP API, Bulk API, Metadata API, Webservices, Informatica</li>
+          <li className="skill">User Interface: Visualforce, Aura Components, Lightning Web Components, SLDS</li>
+          <li className="skill">Reporting: Tableau, Conga, Salesforce Reports/Dashboards</li>
+          <li className="skill">Design Patterns: coding best practices, bulkification, fflib, service classes, test factories, 
 enterprise architecture, scalability, reliability</li>
-          <li>IDE: VSCode, IntelliJ, Eclipse, Developer Console</li>
-          <li>Release: Salesforce CLI, SFDX, CI/CD (Azure DevOps)</li>
-          <li>Agile: Azure Boards, ServiceNow, Workfront, Jira</li>
-          <li>Other Languages: JavaScript, Java, SQL, Excel</li>
+          <li className="skill">IDE: VSCode, IntelliJ, Eclipse, Developer Console</li>
+          <li className="skill">>Release: Salesforce CLI, SFDX, CI/CD (Azure DevOps)</li>
+          <li className="skill">Agile: Azure Boards, ServiceNow, Workfront, Jira</li>
+          <li className="skill">Other Languages: JavaScript, Java, SQL, Excel</li>
         </ul>
       </div>
       <div className="aboutMeComp certifications">
-        <h3>Certifications</h3>
+        <h2>What I've earned...</h2>
+        <ul className="sfCertList">
+          <li>
+            <img className='sfCertIcon' src={SFAdmin} alt='SFAdmin'/>
+          </li>
+          <li>
+            <img className='sfCertIcon' src={SFAdvAdmin} alt='SFAdvAdmin'/>
+          </li>
+          <li>
+            <img className='sfCertIcon' src={SFPlatAppBuilder} alt='SFPlatAppBuilder'/>
+          </li>
+          <li>
+            <img className='sfCertIcon' src={SFDevI} alt='SFDevI'/>
+          </li><li>
+            <img className='sfCertIcon' src={SFDevII} alt='SFDevII'/>
+          </li><li>
+            <img className='sfCertIcon' src={SFJScript} alt='SFJScript'/>
+          </li>
+        </ul>
       </div>
-      <div className="aboutMeComp experience">experience</div>
-      <div className="aboutMeComp hobbiesInterests">hobbies/interests</div>
+      <div className="aboutMeComp experience">Where I've worked...</div>
+      <div className="aboutMeComp hobbiesInterests">Where I've learned...</div>
     </div>
   );
 }
