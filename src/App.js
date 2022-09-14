c505970a8481fef72f1b90ecf4996441ec4b3cd7
Chris Laneville <chris.laneville@fdmgroup.com>
2022-09-13 23:25:20 -0400

    try 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
-import ReactDOM from "react-dom/client";
-import React, {useRef, useEffect, useCallback} from "react";
+//import ReactDOM from "react-dom/client";
+import React, {useEffect} from "react";
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Navbar from "./components/navbar";
 import AboutMe from "./pages/AboutMe";
